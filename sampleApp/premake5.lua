6ff31779b5507073a3e5105f2c7d17a2169cf238
jambuttenshaw <jamie.buttenshaw@gmail.com>
2025-01-17 14:15:51 +0000

    Also moved DXC to D3D12 dependencies

--- sampleApp/premake5.lua
+++ sampleApp/premake5.lua
@@ -39,7 +39,7 @@ project "sampleApp"
 
     postbuildcommands ("{COPYDIR} \"../graphite/bin/" .. outputdir .. "/graphite/\" \"%{cfg.buildtarget.directory}\"\n")
     postbuildcommands ("{COPYDIR} \"../graphite/Platform/D3D12/bin/" .. outputdir .. "/graphiteD3D12/\" \"%{cfg.buildtarget.directory}\"\n")
-    postbuildcommands ("{COPYDIR} \"../graphite/vendor/dxc/bin/x64/\" \"%{cfg.buildtarget.directory}\"")
+    postbuildcommands ("{COPYDIR} \"../graphite/Platform/D3D12/vendor/dxc/bin/x64/\" \"%{cfg.buildtarget.directory}\"")
     
 
     filter "configurations:Debug"
